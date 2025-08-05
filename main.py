6d779ae6550856980dedd0b036fb8dc2b73de9b6
yarik <super.racing@ukr.net>
2025-08-05 14:43:40 +0300

    flake8

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ def main() -> None:
         string = file.read()
         content = json.loads(string)
     for key in content:
-        race, _ = Race.objects.get_or_create(name=content[key]["race"]["name"], description=content[key]["race"]["description"])
+        race, _ = Race.objects.get_or_create(name=content[key]["race"]["name"],
+                                             description=content[key]["race"]["description"])
         for skill in content[key]["race"]["skills"]:
             Skill.objects.get_or_create(name=skill["name"], bonus=skill["bonus"], race=race)
         guild = None
         if content[key].get("guild"):
-            guild, _ = Guild.objects.get_or_create(name=content[key]["guild"]["name"], description=content[key]["guild"]["description"])
-        Player.objects.create(nickname=key, email=content[key]["email"], bio=content[key]["bio"], race=race, guild=guild)
+            guild, _ = Guild.objects.get_or_create(name=content[key]["guild"]["name"],
+                                                   description=content[key]["guild"]["description"])
+        Player.objects.create(nickname=key,
+                              email=content[key]["email"],
+                              bio=content[key]["bio"],
+                              race=race,
+                              guild=guild)
 
 
 if __name__ == "__main__":
