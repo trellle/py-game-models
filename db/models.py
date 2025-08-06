4433dadc6fcf583911684d36bf23d2cbd69a1f04
yarik <super.racing@ukr.net>
2025-08-06 17:18:26 +0300

    fixed foreign keys

--- db/models.py
+++ db/models.py
@@ -17,7 +17,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(unique=True, max_length=255)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
 
 
 class Guild(models.Model):
@@ -28,7 +28,7 @@ class Guild(models.Model):
 class Player(models.Model):
     nickname = models.CharField(unique=True, max_length=255)
     email = models.EmailField(max_length=255, unique=True)
-    bio = models.CharField(max_length=255, null=True)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, null=True, on_delete=models.SET_NULL)
+    bio = models.CharField(max_length=255, blank=True, null=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
+    guild = models.ForeignKey(Guild, null=True, on_delete=models.SET_NULL, related_name="players")
     created_at = models.DateTimeField(auto_now_add=True)
